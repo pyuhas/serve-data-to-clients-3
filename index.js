fb7500686f7d3b8a4e6dde2acb3386a640bb8d17
Pamela <PamelaYuhas@gmail.com>
2018-05-03 16:08:05 -0600

    commit of express server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express     = require('express')
+const bodyParser  = require('body-parser')
+const cors        = require('cors')
+const data        = require('./data.json')
+const app         = express()
+const PORT        = process.env.PORT || 3000
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors({origin: true, credentials: true}))
+
+function filterById(data, id){
+  return data.filter(student => student.ID == id)
+}
+
+app.get('/', (req, res) => res.json(data))
+
+app.get('/:id', (req, res) => {
+  let result = filterById(data, req.params.id)
+  if (!result[0]){
+        res.status(404).json({
+            error: {
+                message: "No record found!"
+            }
+        });
+    } else {
+        res.json({data: result});
+      }
+})
+
+app.listen(PORT, () => console.log('Listening!'))
